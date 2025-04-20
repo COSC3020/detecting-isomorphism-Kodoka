4dea179a77485228fc5eefed2eb5f8736ea47560
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-20 16:56:11 -0600

    Update code.js
    Push to work on at home.

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
-are_isomorphic(graph1, graph2) {
-    return undefined;
+function are_isomorphic(graph1, graph2)
+{
+    var graph1Matrix = [];
+    var graph2Matrix = [];
+
+    return 0;
+}
+
+function permuteGraph(graph)
+{
+    var permutations = [];
+}
+
+var test1 = [[0,1,0,1],
+            [1,0,1,0],
+            [0,1,1,1],
+            [1,0,1,0]];
+
+// Input.
+console.log("Initial: " + JSON.stringify(test1));
+
+// Slices.
+for(var i = 0; i < test1.length; i++)
+{
+    console.log("Slice #" + i + " is " + JSON.stringify(test1.slice(i, i + 1)));
+}
+
+for(var i = 0; i < test1.length; i++)
+{
+    console.log(Array.slice(0, i).)
 }
